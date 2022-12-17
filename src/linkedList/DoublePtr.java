9bd0be68b82947bae9a494e18b03f2e10d104aa5
daijinru <jeocat@163.com>
2022-12-17 17:31:15 +0800

    :sparkles: linkedList loop

--- src/linkedList/DoublePtr.java
+++ src/linkedList/DoublePtr.java
@@ -28,7 +28,58 @@ public class DoublePtr extends ListNode{
     }
 
     /**
-     * Find the entry for the cycle of ListNode.
+     * Find the node for the loop.
+     * Starting running a fast and a slow node, and the fast node speed is 2,
+     * while the fast node catches the other, it can be determined the loop.
      */
+    public ListNode getNodeInLoop(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head.next;
+        ListNode fast = slow.next;
+        while (slow != null && fast != null) {
+            if (slow == fast) return slow;
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null) fast = fast.next;
+        }
+        return null;
+    }
 
+    /**
+     * Find any node in loop, start running until return to the node will get the count of the loop.
+     */
+    public int loopCount(ListNode head) {
+        ListNode nodeInLoop = getNodeInLoop(head);
+        int count = 0;
+        for (ListNode n = nodeInLoop; n.next != nodeInLoop; n = n.next) {
+            count++;
+        }
+        return count;
+    }
+
+    public ListNode getEntryNOde(ListNode head, int loopCount) {
+        ListNode fast = head;
+        for (int i = 0; i < loopCount; i++) {
+            fast = fast.next;
+        }
+        ListNode slow = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public ListNode getEntryNode(ListNode head) {
+        ListNode inLoop = getNodeInLoop(head);
+        if (inLoop == null) return null;
+        ListNode node = head;
+        while (node != inLoop) {
+            node = node.next;
+            inLoop = inLoop.next;
+        }
+        return node;
+    }
+
+    
 }
