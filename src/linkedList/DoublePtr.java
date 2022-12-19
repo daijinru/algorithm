d213a1be1d54f473f6b207e7860acc301d899105
daijinru <jeocat@163.com>
2022-12-19 12:33:27 +0800

    [LinkedList] double ptr: comment

--- src/linkedList/DoublePtr.java
+++ src/linkedList/DoublePtr.java
@@ -142,4 +142,16 @@ public class DoublePtr extends ListNode{
         }
         return count;
     }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
 }
