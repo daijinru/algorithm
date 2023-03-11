30631996e6bfbe6ed0355b7f9d8e8edb551cc302
daijr <jeocat@163.com>
2023-03-11 17:00:30 +0800

    [sort] sleep sort for fun

--- src/utils/ArrayUtil.java
+++ src/utils/ArrayUtil.java
@@ -1,9 +1,18 @@
 package utils;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ArrayUtil {
     public static Integer[] IntsToIntegers(int[] ints) {
         return Arrays.stream(ints).boxed().toArray(Integer[]::new);
+    }
+
+    public static int[] IntegersToInts(Integer[] integers) {
+        return Arrays.stream(integers).mapToInt(Integer::valueOf).toArray();
+    }
+
+    public static int[] ListIntegerToInts(List<Integer> ins) {
+        return ins.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
